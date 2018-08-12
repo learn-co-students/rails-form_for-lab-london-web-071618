ab65b097b6de70bcfcd641f0276f4aa365046419
Flatiron School <flatiron@Flatirons-MacBook-Air.local>
2018-08-12 19:20:12 +0100

    Jo

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,19 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  get '/students', to: "students#index"
+  get '/students/new', to: "students#new", as: 'new_student'
+  get '/students/:id/edit', to: "students#edit", as: 'edit_student'
+  get '/students/:id', to: "students#show", as: 'student'
+  post '/students', to: "students#create"
+  patch '/students/:id', to: 'students#update'
+
+  get '/school_classes', to: "school_classes#index"
+  get '/school_classes/new', to: "school_classes#new", as: 'new_school_class'
+  get '/school_classes/:id/edit', to: "school_classes#edit", as: 'edit_school_class'
+  get '/school_classes/:id', to: "school_classes#show", as: 'school_class'
+  post '/school_classes', to: "school_classes#create"
+  patch '/school_classes/:id', to: 'school_classes#update'
+
+
 end
